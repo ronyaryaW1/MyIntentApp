765eddeccc547043a2c3ea059fa77a7117649805
ronyaryaW1 <bangwijay11@gmail.com>
2019-08-02 01:24:13 -0700

    firstCommit

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveDataWithActivity = findViewById(R.id.btn_move_activity_data);
+        btnMoveDataWithActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "RONAYYY");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 17);
+                startActivity(moveWithDataIntent);
+                break;
+            
+        }
+    }
+}
+
+
